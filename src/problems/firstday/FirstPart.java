91a34884b01444f471ac93d30e70975dd21b6b1e
ulasonmez <ulasonmez@outlook.com>
2022-03-03 19:31:59 +0300

    first day

--- src/problems/firstday/FirstPart.java
+++ src/problems/firstday/FirstPart.java
@@ -1,4 +1,4 @@
-package firstday;
+package problems.firstday;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class SecondPart {
+public class FirstPart {
 	public static void main(String[] args) throws IOException{
 		File file = new File("firstinput.txt");
 		FileReader fr = new FileReader(file);
@@ -16,14 +16,11 @@ public class SecondPart {
 		while ((st = br.readLine()) != null) {
 			inputNumbers.add(Integer.parseInt(st));
 		}
-		int beforeSum = 0;
 		int increased = 0;
-		for(int i = 0 ; i<inputNumbers.size()-2;i++) {
-			int sum = inputNumbers.get(i)+inputNumbers.get(i+1)+inputNumbers.get(i+2);
-			if(sum>beforeSum && beforeSum!=0) {
-				increased+=1;
+		for(int i = 1 ; i<inputNumbers.size();i++) {
+			if(inputNumbers.get(i)>inputNumbers.get(i-1)) {
+				increased += 1;
 			}
-			beforeSum = sum;
 		}
 		System.out.println(increased);
 	}
